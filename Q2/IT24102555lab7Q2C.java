c951f1d0091492d49821098e1b03dcb3ec1ff732
IT24102555 <it24102555@my.sliit.lk>
2024-09-17 21:58:18 -0700

    Add files via upload

---
+++ Q2/IT24102555lab7Q2C.java
@@ -0,0 +1,15 @@
+public class IT24102555lab7Q2C {
+    public static void main(String[] args) {
+        int i,j;  
+        
+  
+        for (i = 5; i >= 1; i--) {
+            
+            for (j = i; j >= 1; j--) {
+                System.out.print(i);
+            }
+            
+            System.out.println();
+        }
+    }
+}
